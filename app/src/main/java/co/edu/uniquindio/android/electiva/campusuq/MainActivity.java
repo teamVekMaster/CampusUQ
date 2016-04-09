a7b2fa1f748198a280fe408926ea28d5564f7467
Capriatto <michu-1@hotmail.com>
2016-04-09 12:35:52 -0500

    proyecto inicial -Capriatto

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/campusuq/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.android.electiva.campusuq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
